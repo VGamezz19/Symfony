04cd7bb90e7bf9f01957f27c31412d73e8eddea3
Victor.Gamez <pictor.games30@gmail.com>
2016-11-04 18:17:22 +0100

    calculadora 04/11/2016

--- my_project_name/src/AppBundle/Controller/CalculatorController.php
+++ my_project_name/src/AppBundle/Controller/CalculatorController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 class CalculatorController extends Controller
 {
     /**
-     * @Route("/", name="app_calculator_index")
+     * @Route("/calculadora", name="app_calculator_index")
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
@@ -17,14 +17,43 @@ class CalculatorController extends Controller
         return $this->render(':Calculator:Index.html.twig');
     }
     /**
-     * @Route("/suma/{num1}/{num2}", name="app_calculator_sum")
+     * @Route("/suma", name="app_calculator_sum")
      *
      */
     public function sumAction($num1, $num2)
     {
+       /*  */
+
+        return $this->render(':Calculator:Form.html.twig' ,
+            [
+            'action' => 'app_calculator_doSum'
+        ]);
+
+    }
+
+    /**
+     * @Route("/duSum", name="app_calculator_doSum")
+     *
+     */
+    public function doSum(Request $request)
+    {
+        /*$num1= $_POST['op1'];*/
+        $num1= $request->request->get('op1');
+
+        /*$num2= $_POST['op2'];*/
+        $num2= $request->request->get('op2');
+
         $n1 =  new Calculator($num1, $num2);
         $n1->sum();
-        return new Response($n1->getRes());
+        $result = $n1->getRes();
 
+        return $this->render(':Calculator:Result.html.twig',
+            [
+                'titulo'    => "Suma",
+                'result'    => $result,
+                'op1'       => $num1,
+                'op2'       => $num2,
+                'operacion' => "+"
+            ]);
     }
 }
