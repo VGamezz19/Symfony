77ed20d8508a5f2844b399bfbd51991e068b9d2a
Victor.Gamez <pictor.games30@gmail.com>
2016-11-04 15:49:19 +0100

    Commit 04/11/2016

--- my_project_name/src/AppBundle/Controller/CalculatorController.php
+++ my_project_name/src/AppBundle/Controller/CalculatorController.php
@@ -3,23 +3,28 @@ namespace AppBundle\Controller;
 use AppBundle\Service\Calculator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 class CalculatorController extends Controller
 {
     /**
      * @Route("/", name="app_calculator_index")
-     * @param $name
+     *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction($name)
+    public function indexAction()
     {
         return $this->render(':Calculator:Index.html.twig');
     }
     /**
-     * @Route("/suma/{num1}/{num2}", name"app_calculator_sum")
+     * @Route("/suma/{num1}/{num2}", name="app_calculator_sum")
+     *
      */
-    public function indexputitaAction($num1, $num2)
+    public function sumAction($num1, $num2)
     {
         $n1 =  new Calculator($num1, $num2);
-        return $this->n1.sum();
+        $n1->sum();
+        return new Response($n1->getRes());
+
     }
 }
